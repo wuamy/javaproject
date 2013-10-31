737ea5bf9e31198ec8b896b9a14feb8413d1609c
amy <wuamyamy@gmail.com>
2013-10-30 21:03:00 -0400

    update

---
+++ amylight/src/count/HelloWorld.java
@@ -0,0 +1,42 @@
+package count;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		ServletOutputStream out = response.getOutputStream();
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("<h1>Hello World</h1>");
+		out.println("");
+		
+	}
+
+	
+
+}
